5df851bfcc8fa06c8449a4df083a285d0db7ec30
Sixtus Opara <sixtusopara@macs-MacBook-Pro.local>
2023-12-31 04:46:21 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+(function($) {
+    "use strict";
+
+
+    /*-------------------------------------
+    On Load
+    -------------------------------------*/
+    $(window).on('load resize', function() {
+
+        $('body').imagesLoaded().done(function(instance) {
+            $('body').addClass('loaded');
+        });
+    });
+
+
+
+    /*-------------------------------------
+    Background image
+    -------------------------------------*/
+    $("[data-bg-image]").each(function() {
+        var img = $(this).data("bg-image");
+        $(this).css({
+            backgroundImage: "url(" + img + ")"
+        });
+    });
+  
+
+})(jQuery);
